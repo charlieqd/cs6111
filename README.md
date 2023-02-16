815044c282f8e81908fb898d5a25b3315b3b9474
Chenhao Liu <liuchenhaoqd@gmail.com>
2023-02-15 19:50:41 -0500

    Add simple custom search api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cs6111
+Search Engine ID: a95a1235f40abcb47
+Custom Search API Key: AIzaSyCMb6xd4NnbwRHgXUPVA5dTfFxaAN3CuLE
---
+++ query.py
@@ -0,0 +1,23 @@
+import sys
+import requests
+from googleapiclient.discovery import build
+
+
+def main():
+    api_key = sys.argv[1]
+    engine_id = sys.argv[2]
+    precision = sys.argv[3]
+    query = sys.argv[4].lower()
+
+    print("parameters are ", api_key, engine_id, precision)
+    print(query)
+
+    service = build("customsearch", "v1", developerKey=api_key)
+    resp = service.cse().list(q=query, cx=engine_id).execute()
+    items = resp['items']
+    for item in items:
+        print(item.get('link'))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
