93ac3bb9ded2f48b5ecc97fcbcdb44313ef7613a
jzhzhz <monsttrod@gmail.com>
2023-02-15 23:24:40 -0500

    basic proj structure

--- query.py
+++ query.py
@@ -3,23 +3,51 @@ import pprint
 from googleapiclient.discovery import build
 
 
+def augment():
+    #TODO do augment here
+    return None
+
 def main():
     api_key = sys.argv[1]
     engine_id = sys.argv[2]
     precision = sys.argv[3]
     query = sys.argv[4].lower()
 
-    print("parameters are ", api_key, engine_id, precision)
-    print(query)
-
     service = build("customsearch", "v1", developerKey=api_key)
     resp = service.cse().list(q=query, cx=engine_id).execute()
     # pprint.pprint(resp)
     items = resp['items']
-    for item in items:
-        print("Title: ", item.get('title'))
-        print("Link: ", item.get('link'))
-        print("Description: ", item.get('snippet'))
+    rel = 0
+    while rel < float(precision):
+        count = 0
+        rel = 0
+
+        print("Parameters: ", api_key, engine_id, precision)
+        print("API key      = " + api_key)
+        print("Engine Key   = " + engine_id)
+        print("Precision    = " + precision)
+        print("Query        = " + query)
+        print("Google Search Results:")
+        print("========================")
+
+        for i, item in enumerate(items):
+            print("Result " + str(i+1))
+            print("Title: ", item.get('title'))
+            print("Link: ", item.get('link'))
+            print("Description: ", item.get('snippet'))
+            feedback = input("Relevant? (Y/N)")
+            if feedback == "Y":
+                count += 1
+        rel = count / 10
+
+        print("========================")
+        print("FEEDBACK SUMMARY")
+        print("Query: " + query)
+        print("Precision: " + str(rel) + " < " + str(precision))
+        # TODO step 4
+        augment()
+        print("Augmented by ...")
+
 
 
 if __name__ == '__main__':
